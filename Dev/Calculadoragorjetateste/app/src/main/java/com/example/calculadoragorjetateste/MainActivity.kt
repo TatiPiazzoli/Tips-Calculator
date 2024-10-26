bd2ccbb933f61b5976f7248ffcaed178124e8755
TatiPiazzoli <tatianamenpia@gmail.com>
2024-10-26 17:28:29 +0100

    Cálculo tips e SnackBar

--- Dev/Calculadoragorjetateste/app/src/main/java/com/example/calculadoragorjetateste/MainActivity.kt
+++ Dev/Calculadoragorjetateste/app/src/main/java/com/example/calculadoragorjetateste/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.calculadoragorjetateste.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
@@ -16,15 +17,55 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val Btnclean: Button = findViewById(R.id.btn_clean)
-        val Btncalculate: Button = findViewById(R.id.btn_calculate)
-        val Totalamount: TextInputEditText = findViewById(R.id.tiet_amount)
-        val Totalpeople: TextInputEditText = findViewById(R.id.tiet_people)
-        val Percentageten: RadioButton = findViewById(R.id.rb_ten)
-        val Percentagefifteen: RadioButton = findViewById(R.id.rb_fifteen)
-        val Percentagetwenty: RadioButton = findViewById(R.id.rb_twenty)
+        var percentage: Int = 0
 
+        binding.rbTen.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked){
+                percentage = 10
+            }
+        }
+
+        binding.rbFifteen.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked){
+                percentage = 15
+            }
+        }
+
+        binding.rbTwenty.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked){
+                percentage = 20
+            }
+        }
+
+        binding.btnClean.setOnClickListener {
+
+        }
+
+        binding.btnCalculate.setOnClickListener {
+
+            val totalAmountTemp = binding.tietAmount.text
+            val peopleTemp = binding.tietPeople.text
+
+            if (totalAmountTemp?.isEmpty() == true ||
+                peopleTemp?.isEmpty() == true
+            ) {
+
+                Snackbar.make(binding.tietAmount, "Please fill in all fields", Snackbar.LENGTH_LONG)
+                    .show()
+
+            } else {
+
+                val totalAmount: Float = totalAmountTemp.toString().toFloat()
+                val people: Int = peopleTemp.toString().toInt()
+
+                val totalTemp = totalAmount / people
+                val tips = totalTemp * percentage / 100
+                val totalTips = totalTemp + tips
+                binding.tvResult.text = "Total with tips:$totalTips"
+            }
+        }
     }
 }
