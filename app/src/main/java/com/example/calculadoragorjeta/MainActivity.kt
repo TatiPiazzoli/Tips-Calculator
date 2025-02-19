2acd636c2ff1c9cf3a1d5434705a1f8a601fb6c0
TatiPiazzoli <tatianamenpia@gmail.com>
2025-02-19 15:33:57 +0000

    Utilizando Binding para recuperar as Views

--- app/src/main/java/com/example/calculadoragorjeta/MainActivity.kt
+++ app/src/main/java/com/example/calculadoragorjeta/MainActivity.kt
@@ -1,20 +1,51 @@
 package com.example.calculadoragorjeta
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.calculadoragorjeta.databinding.ActivityMainBinding
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
+
+
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var percentage: Int = 0
+
+        binding.rbTen.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked){
+                percentage = 10
+            }
+        }
+        binding.rbFifteen.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked){
+                percentage = 15
+            }
+        }
+        binding.rbTwenty.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked){
+                percentage = 20
+            }
+        }
+
+        binding.btnClean.setOnClickListener {
+
+        }
+
+        binding.btnCalculate.setOnClickListener {
+            val totalAmount: Float = binding.tieTotal.text.toString().toFloat()
+            val nPeople: Float = binding.tieNumPeople.text.toString().toFloat()
         }
     }
 }
